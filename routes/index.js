df9feb47bd64c217edb4ab01a9fc1d8f7fbd925b
webcup-athack <69953669+tsantaniainarakotonjanahary@users.noreply.github.com>
2023-08-04 04:12:13 +0300

    go

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/hello', function(req, res, next) { res.send('Tongasoa eto @ api '); });
+router.get('/hello', function(req, res, next) { res.send('Tongasoa o '); });
 
 module.exports = router;
